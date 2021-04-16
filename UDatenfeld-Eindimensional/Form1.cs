7478d9cd538c997404f2a061aa5fe3903d1c1bc3
Paddo <82466215+Patrick748@users.noreply.github.com>
2021-04-16 18:01:02 +0200

    Aus Kapitel 4.4.4 Datenfelder
    Funktioniert.

---
+++ UDatenfeld-Eindimensional/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UDatenfeld_Eindimensional
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private readonly Random r = new Random();
+
+        private void BtnMin_Click(object sender, EventArgs e)
+        {
+            int[] zahlen = new int[10];
+            
+            LstZahlen.Items.Clear();
+
+            for(int i =0; i<= zahlen.GetUpperBound(0); i++)
+            {
+                zahlen[i] = r.Next(0,10);
+                LstZahlen.Items.Add(zahlen[i]);
+            }
+
+            int minimalwert = zahlen[0];
+
+            for (int i=0; i <= zahlen.GetUpperBound(0); i++)
+            {
+                if(minimalwert > zahlen[i])
+                {
+                    minimalwert = zahlen[i];
+                }
+            }
+
+            LblAusgabe.Text = "Minimalwert: " + minimalwert + "\n";
+
+            for (int i = 0; i <= zahlen.GetUpperBound(0); i++)
+            {
+                if (minimalwert >= zahlen[i])
+                {
+                    LblAusgabe.Text += "Index: " + i + "\n";
+                }
+            }
+        }
+    }
+}
